55143e8901dd3f3f7ef306786c5573283666db0e
romeohuy <romeohuy@gmail.com>
2021-06-01 08:08:26 +0700

    Initial git again

--- src/BdhCMS.Web/ServicesMail/ISmtpService.cs
+++
@@ -1,9 +0,0 @@
-﻿using BdhCMS.Web.Models;
-namespace BdhCMS.Web.ServicesMail
-{
-    public interface ISmtpService
-    {
-        string SendContactToEmail(ContactFormViewModel model, string bodyEmail);
-        string SendApplyJobEmail(ApplyJobFormViewModel model, string bodyEmail);
-    }
-}
